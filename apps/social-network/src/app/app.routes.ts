d3a09801470c2a5ea59cbf2babd36644b0121eb8
Devashish Sarmah <devashishSarmah@Vantages-MacBook-Air-4.local>
2024-04-10 00:49:06 +0530

    feat(home): setup not found page and home page

--- apps/social-network/src/app/app.routes.ts
+++ apps/social-network/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const routesMap = {
+    AUTH: 'auth'
+}
+
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        redirectTo: routesMap.AUTH,
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        loadComponent: () => import('./pages/not-found/not-found.component').then(c => c.NotFoundComponent)
+    }
+];
